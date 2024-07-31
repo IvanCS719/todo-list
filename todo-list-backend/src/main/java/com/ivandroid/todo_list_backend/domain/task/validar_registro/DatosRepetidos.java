1ffe87713a42f2eba573101bc73fb8f9ac799225
IvanCS719 <ivancupil17@gmail.com>
2024-07-31 15:51:51 -0600

    Agregando validaciones para el registro de task

---
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/task/validar_registro/DatosRepetidos.java
@@ -0,0 +1,20 @@
+package com.ivandroid.todo_list_backend.domain.task.validar_registro;
+
+import com.ivandroid.todo_list_backend.domain.task.RegistroTaskDTO;
+import com.ivandroid.todo_list_backend.domain.task.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DatosRepetidos implements IValidarRegistro{
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Override
+    public void validar(RegistroTaskDTO datosRegistro) {
+        //Buscar titulo existente
+        var tituloRepetido = taskRepository.existsByTitleIgnoreCase(datosRegistro.title());
+
+        if (tituloRepetido) throw new RuntimeException("Título de la tarea duplicado");
+
+    }
+}
---
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/task/validar_registro/IValidarRegistro.java
@@ -0,0 +1,7 @@
+package com.ivandroid.todo_list_backend.domain.task.validar_registro;
+
+import com.ivandroid.todo_list_backend.domain.task.RegistroTaskDTO;
+
+public interface IValidarRegistro {
+    public void validar(RegistroTaskDTO datosRegistro);
+}
