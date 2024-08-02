49c6913b445a6ea319a1f30a065c3a525a6ca04e
IvanCS719 <ivancupil17@gmail.com>
2024-08-02 14:01:55 -0600

    Agregando constructor al DTO de respuesta del task

--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/task/RespuestaTaskDTO.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/task/RespuestaTaskDTO.java
@@ -2,9 +2,18 @@ package com.ivandroid.todo_list_backend.domain.task;
 
 import java.time.LocalDateTime;
 
-public record RespustaTaskDTO(
+public record RespuestaTaskDTO(
+        Long id,
         String title,
         String description,
         LocalDateTime dueDate
 ) {
+    public RespuestaTaskDTO(Task task) {
+        this(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate()
+        );
+    }
 }
