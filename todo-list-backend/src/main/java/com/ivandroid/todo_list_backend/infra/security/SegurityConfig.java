fef839560ed5ababb24c30711190c108a632a97a
IvanCS719 <ivancupil17@gmail.com>
2024-08-16 13:01:22 -0600

    Se crea el filtro de seguridad para permitir y bloquear ciertas rutas del api

---
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/infra/security/SegurityConfig.java
@@ -0,0 +1,30 @@
+package com.ivandroid.todo_list_backend.infra.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SegurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(withDefaults())
+                .build();
+    }
+}
