2c6094a990537bbb6dee1a32661c9eb1b4b4cf54
IvanCS719 <ivancupil17@gmail.com>
2024-08-23 13:20:12 -0600

    Se agregó el RespondeEntity para los controladores de login y registro

--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/controller/auth/AuthController.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/controller/auth/AuthController.java
@@ -1,7 +1,15 @@
 package com.ivandroid.todo_list_backend.controller.auth;
 
+import com.ivandroid.todo_list_backend.infra.security.jwt.RespuestaJwtDTO;
+import com.ivandroid.todo_list_backend.domain.user.LoginRequestDTO;
+import com.ivandroid.todo_list_backend.domain.user.RegisterRequestDTO;
+import com.ivandroid.todo_list_backend.infra.security.AuthService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthController {
 
+    @Autowired
+    private final AuthService authService;
+
     @PostMapping("/login")
-    public String login(){
-        return "Te has logueado";
+    public ResponseEntity<RespuestaJwtDTO> login(@RequestBody @Valid LoginRequestDTO request){
+        return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping("/register")
-    public String register(){
-        return "Te has registrado";
+    public ResponseEntity<RespuestaJwtDTO> register(@RequestBody @Valid RegisterRequestDTO request){
+        return ResponseEntity.ok(authService.register(request));
     }
 }
