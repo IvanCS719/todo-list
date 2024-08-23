73d50c7ddf27b88e5e6d740a126cd2cb1ead9064
IvanCS719 <ivancupil17@gmail.com>
2024-08-23 14:59:21 -0600

    Cambiando la autenticacion por defecto a JWT

--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/infra/security/SegurityConfig.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/infra/security/SegurityConfig.java
@@ -1,19 +1,24 @@
 package com.ivandroid.todo_list_backend.infra.security;
 
+import com.ivandroid.todo_list_backend.infra.security.jwt.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SegurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
@@ -24,7 +29,11 @@ public class SegurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .formLogin(withDefaults())
+                .sessionManagement( sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
