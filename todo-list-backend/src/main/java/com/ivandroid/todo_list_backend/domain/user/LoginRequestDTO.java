6a45695cd43cb1792eed5415a459b754898de8c1
IvanCS719 <ivancupil17@gmail.com>
2024-08-23 12:57:03 -0600

    Eliminacion de comentario innecesarios

--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/user/LoginRequestDTO.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/user/LoginRequestDTO.java
@@ -2,14 +2,6 @@ package com.ivandroid.todo_list_backend.domain.user;
 
 import jakarta.validation.constraints.NotNull;
 
-/*@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class LoginRequestDTO {
-    String email;
-    String password;
-}*/
 public record LoginRequestDTO(
         @NotNull
         String email,
--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/user/RegisterRequestDTO.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/domain/user/RegisterRequestDTO.java
@@ -1,15 +1,5 @@
 package com.ivandroid.todo_list_backend.domain.user;
 
-/*@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class RegisterRequestDTO {
-    String username;
-    String email;
-    String password;
-}*/
-
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequestDTO(
