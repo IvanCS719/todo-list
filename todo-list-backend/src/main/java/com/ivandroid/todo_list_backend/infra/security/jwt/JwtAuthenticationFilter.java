a366574408507efc492dde8bc3e7a37d2f8ab093
IvanCS719 <ivancupil17@gmail.com>
2024-08-23 15:01:53 -0600

    Validando el token para el login

--- todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/infra/security/jwt/JwtAuthenticationFilter.java
+++ todo-list-backend/src/main/java/com/ivandroid/todo_list_backend/infra/security/jwt/JwtAuthenticationFilter.java
@@ -1,33 +1,55 @@
 package com.ivandroid.todo_list_backend.infra.security.jwt;
 
+import com.ivandroid.todo_list_backend.domain.user.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@AllArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Autowired
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    private  final UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        final String token = getTokenFromRequest(request);
-
-        /*if(token == null){
-            filterChain.doFilter(request, response);
-            return;
+        final String authToken = getTokenFromRequest(request);
+        if (authToken != null){
+            var subject = jwtService.getSubject(authToken);
+            if(subject != null){
+                //Token valido
+                var usuario = userRepository.findByEmail(subject);
+                if(usuario.isPresent()){
+                    var authetication = new UsernamePasswordAuthenticationToken(usuario.get(),
+                            null, usuario.get().getAuthorities()); //Forzar inicio de sesión
+                    SecurityContextHolder.getContext().setAuthentication(authetication);
+                }
+            }
         }
-
-        filterChain.doFilter(request, response);*/
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
